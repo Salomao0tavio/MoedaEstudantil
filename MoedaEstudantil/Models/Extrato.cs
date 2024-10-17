f3777652c54b792abeb7af5ece3386619a24be43
OtavioSalomao <otavio.ferreira.mds@gmail.com>
2024-10-16 22:06:28 -0300

    Add Extrato class to store total coins and transactions

---
+++ MoedaEstudantil/Models/Extrato.cs
@@ -0,0 +1,10 @@
+﻿using MoedaEstudantil.Entities;
+
+namespace MoedaEstudantil.Models
+{
+    public class Extrato
+    {
+        public decimal TotalMoedas { get; set; }
+        public List<Transacao> Transacoes { get; set; }
+    }
+}
