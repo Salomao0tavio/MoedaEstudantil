f26a72c6229bffd79f946981ba67d2f124751ad5
OtavioSalomao <otavio.ferreira.mds@gmail.com>
2024-10-16 22:11:29 -0300

    Refactor Startup.cs to configure SMTP settings and add service classes

--- MoedaEstudantil/Startup.cs
+++ MoedaEstudantil/Startup.cs
@@ -1,51 +1,59 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using MoedaEstudantil.Data;
+using MoedaEstudantil.Models;
 using MoedaEstudantil.Services;
 
-namespace MoedaEstudantil
+public class Startup
 {
-    public class Startup
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
     {
-        public IConfiguration Configuration { get; }
+        Configuration = configuration;
+    }
 
-        public Startup(IConfiguration configuration)
+    // Configura os serviços
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<MeritSystemContext>(options =>
+            options.UseMySQL(Configuration.GetConnectionString("DefaultConnection")));
+
+        // Registra as configurações de Email
+        services.Configure<EmailSettings>(Configuration.GetSection("EmailSettings"));
+
+        // Configura o EmailService para injetar as configurações do appsettings.json
+        services.AddScoped<EmailService>(sp =>
         {
-            Configuration = configuration;
+            var emailSettings = sp.GetRequiredService<IOptions<EmailSettings>>().Value;
+            return new EmailService(emailSettings.SmtpHost, emailSettings.SmtpPort, emailSettings.SmtpUsername, emailSettings.SmtpPassword);
+        });
+
+        services.AddScoped<AlunoService>();
+        services.AddScoped<EmpresaService>();
+        services.AddScoped<ProfessorService>();
+        services.AddScoped<VantagemService>();
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+    }
+
+    // Configura o pipeline HTTP
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
 
-        // Configura os serviços
-        public void ConfigureServices(IServiceCollection services)
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthorization();
+        app.UseEndpoints(endpoints =>
         {
-            services.AddDbContext<MeritSystemContext>(options =>
-                options.UseMySQL(Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddScoped<AlunoService>();
-            services.AddScoped<EmpresaService>();
-            services.AddControllers();
-            services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
-        }
-
-        // Configura o pipeline HTTP
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+            endpoints.MapControllers();
+        });
     }
 }
